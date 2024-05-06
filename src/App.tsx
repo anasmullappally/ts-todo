c48f4755acc04c14de0d8090e5133800c4b05cac
Anas Mullappally <101805320+anasmullappally@users.noreply.github.com>
2024-05-06 12:56:50 +0530

    base ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
+import { MdDelete } from 'react-icons/md'
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
 
   return (
     <>
-      <div>
+      <div className='h-[90vh] flex flex-col justify-center items-center'>
+        <Header />
+        <div className='w-[350px]'>
+          <form className='mb-5'>
+            <input type="text" className='w-full p-2 rounded-sm mb-2' />
+            <button type='submit' className='bg-gray-700 w-full p-2'> Add</button>
+          </form>
+          <div className='bg-orange-800 h-52 overflow-y-auto'>
+            <div className='flex justify-between items-center border border-slate-600 pl-2 mb-2 mr-1'>
+              <p>Hello</p>
+              <button>
+                <MdDelete />
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+export const Header = () => {
+    return (
+        <div className="flex items-center mb-8">
+            <img
+                className="w-28 h-28 object-contain "
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/React_Logo_SVG.svg/1111px-React_Logo_SVG.svg.png"
+                alt="react" />
+            <span className="mx-4 ">+</span>
+            <img
+                className="w-28 h-28 object-contain "
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/768px-Typescript_logo_2020.svg.png"
+                alt="ts" />
+        </div>
+    )
+}
